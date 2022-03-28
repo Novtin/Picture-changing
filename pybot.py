b6c6785474081e30ed6905bbcb41399b28dfb332
Yarik <yarichok.2019@bk.ru>
2022-03-28 19:39:36 +0700

    Black and White filter

--- pybot.py
+++ pybot.py
@@ -1,40 +1,52 @@
 #!venv/bin/python
 import logging
+import cv2
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.dispatcher.filters.state import StatesGroup, State
 
-# Объект бота
-bot = Bot(token="5208126996:AAGgbK5tyQ6UtNAvV6I56Asct6adKbGEPMY")
-# Диспетчер для бота
-dp = Dispatcher(bot)
-# Включаем логирование, чтобы не пропустить важные сообщения
-logging.basicConfig(level=logging.INFO)
 
-# кнопка после старта
-button_start = KeyboardButton('Чёрно-белый 🔳')
+class Status(StatesGroup):
+    F1 = State()
+
+
+bot = Bot(token="5208126996:AAGgbK5tyQ6UtNAvV6I56Asct6adKbGEPMY")  # Объект бота
+dp = Dispatcher(bot)  # Диспетчер для бота
+logging.basicConfig(level=logging.INFO)  # Включаем логирование, чтобы не пропустить важные сообщения
+
+button_start = KeyboardButton('Чёрно-белый 🔳')  # кнопка после старта
 
 greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 greet_kb.add(button_start)
 
-# Хэндлер на команду /test1
 
-
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start'], state=None)  # Команда старт
 async def start(message: types.Message):
     await message.answer("Выберите стиль для изменения", reply_markup=greet_kb)
 
 
-@dp.message_handler()
+@dp.message_handler(state=Status.F1)
+async def send_photo(message: types.Message):
+    if message.content_type == 'photo':
+        await message.photo[-1].download('test.jpg')
+
+        img_grey = cv2.imread('test.jpg', cv2.IMREAD_GRAYSCALE)
+        color = 128
+        img_binary = cv2.threshold(img_grey, color, 255, cv2.THRESH_BINARY)[1]
+        cv2.imwrite('test_BaW.jpg', img_binary)
+
+        photo = open('test_BaW.jpg', 'rb')
+        await bot.send_photo(message.from_user.id, photo=photo, caption="Результат")
+    else:
+        await message.answer('ops')
+
+
+@dp.message_handler(state=None)  # Функция стиля
 async def next_start(message: types.Message):
     if message.text == 'Чёрно-белый 🔳':
         await message.answer("Загрузите фотографию", reply_markup=ReplyKeyboardRemove())
-
-
-@dp.message_handler(content_types=["photo"])
-async def send_photo(message: types.Message):
-    photo_id = message.photo[-1].file_id
-    await bot.send_photo(message.from_user.id, photo_id, caption='Результат')
+        await Status.first()
 
 
 if __name__ == "__main__":

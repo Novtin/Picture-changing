d15342f92c4b088d268adf5c650f14479e151980
Danil <danil_ustinov@mail.ru>
2022-03-28 22:04:59 +0700

    Нормальный уже такой бот

--- pybot.py
+++ pybot.py
@@ -1,53 +1,46 @@
-#!venv/bin/python
-import logging
 import cv2
+import logging
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-class Status(StatesGroup):
-    F1 = State()
-
-
-bot = Bot(token="5208126996:AAGgbK5tyQ6UtNAvV6I56Asct6adKbGEPMY")  # Объект бота
-dp = Dispatcher(bot)  # Диспетчер для бота
+bot = Bot(token="5208126996:AAGgbK5tyQ6UtNAvV6I56Asct6adKbGEPMY", parse_mode=types.ParseMode.HTML)  # Объект бота
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)  # Диспетчер для бота
 logging.basicConfig(level=logging.INFO)  # Включаем логирование, чтобы не пропустить важные сообщения
 
-button_start = KeyboardButton('Чёрно-белый 🔳')  # кнопка после старта
+button_start1 = KeyboardButton('Чёрно-белый 🔳') # кнопка после старта
+button_start2 = KeyboardButton('Пиксель - арт') # кнопка после старта
 
 greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-greet_kb.add(button_start)
+greet_kb.add(button_start1, button_start2)
 
 
-@dp.message_handler(commands=['start'], state=None)  # Команда старт
+@dp.message_handler(commands=['start'])  # Команда старт
 async def start(message: types.Message):
     await message.answer("Выберите стиль для изменения", reply_markup=greet_kb)
 
 
-@dp.message_handler(state=Status.F1)
-async def send_photo(message: types.Message):
-    if message.content_type == 'photo':
-        await message.photo[-1].download('test.jpg')
-
-        img_grey = cv2.imread('test.jpg', cv2.IMREAD_GRAYSCALE)
-        color = 128
-        img_binary = cv2.threshold(img_grey, color, 255, cv2.THRESH_BINARY)[1]
-        cv2.imwrite('test_BaW.jpg', img_binary)
-
-        photo = open('test_BaW.jpg', 'rb')
-        await bot.send_photo(message.from_user.id, photo=photo, caption="Результат")
-    else:
-        await message.answer('ops')
-
-
-@dp.message_handler(state=None)  # Функция стиля
-async def next_start(message: types.Message):
+@dp.message_handler()  # Функция стиля
+async def next_start(message: types.Message, state: FSMContext):
     if message.text == 'Чёрно-белый 🔳':
         await message.answer("Загрузите фотографию", reply_markup=ReplyKeyboardRemove())
-        await Status.first()
+        await state.set_state('wb')
 
+
+@dp.message_handler(state='wb', content_types=['photo'])
+async def send_photo(message: types.Message, state: FSMContext):
+    await message.photo[-1].download('test.jpg')
+    img_grey = cv2.imread('test.jpg', cv2.IMREAD_GRAYSCALE)
+    color = 128
+    img_binary = cv2.threshold(img_grey, color, 255, cv2.THRESH_BINARY)[1]
+    cv2.imwrite('test_BaW.jpg', img_binary)
+    photo = open('test_BaW.jpg', 'rb')
+    await bot.send_photo(message.from_user.id, photo=photo, caption="Результат")
+    await state.finish()
 
 if __name__ == "__main__":
     # Запуск бота
